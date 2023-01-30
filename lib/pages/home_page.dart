ea3084804bcd1c3c50db7e459abfe5a54e826a22
Viganesh <viganeshgulekar2205@gmail.com>
2023-01-30 18:59:49 +0530

    initail commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
